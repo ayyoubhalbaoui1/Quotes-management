3c9cbc1f67ec47838c2dcda152547d501cc09faf
ayyoubhalbaoui1 <ayyoubhalbaoui1@gmail.com>
2020-04-17 19:28:34 +0100

    mod

---
+++ 1st_db.sql
@@ -0,0 +1,13 @@
+
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(20) DEFAULT NULL,
+  `prenom` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+
